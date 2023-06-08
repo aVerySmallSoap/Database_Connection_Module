2599b2ad39c450c659547ef44bbba9978b03885f
Lirys <iamyourmarkieboo@gmail.com>
2023-06-08 19:38:26 +0800

    Shortened the if check for easier readability

--- src/ConnectionService.java
+++ src/ConnectionService.java
@@ -18,11 +18,9 @@ public class ConnectionService implements Connector {
     @Override
     public Connection getConnection(String URL, String user, String pass){
         try{
-            if((user == null || user.equals("")) && (pass == null || pass.equals(""))){
-                return DriverManager.getConnection(URL, "root", "root");
-            }else{
-            return DriverManager.getConnection(URL, user, pass);
-            }
+            return user == null || user.equals("") && pass == null || pass.equals("") ?
+                    DriverManager.getConnection(URL, "root", "root") :
+                    DriverManager.getConnection(URL, user, pass);
         }catch (SQLException e){
             throw new RuntimeException(e.getMessage());
         }
