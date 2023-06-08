828f70d2e581fe12303f2f662ddd339f4041167a
Lirys <iamyourmarkieboo@gmail.com>
2023-06-08 16:35:03 +0800

    Built a jar file for ease-of-use

--- src/ConnectionService.java
+++ src/ConnectionService.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class ConnectionService {
+import Interface.Connector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@SuppressWarnings("unused")
+public class ConnectionService implements Connector {
+    @Override
+    public Connection getConnection(String URL) {
+        try{
+            return DriverManager.getConnection(URL);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public Connection getConnection(String URL, String user, String pass){
+        try{
+            return DriverManager.getConnection(URL, user, pass);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
--- src/Interface/Connector.java
+++ src/Interface/Connector.java
@@ -1,2 +1,10 @@
-package Interface;public interface Connector {
+package Interface;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SuppressWarnings("unused")
+public interface Connector {
+    Connection getConnection(String URL) throws SQLException;
+    Connection getConnection(String URL, String user, String pass) throws SQLException;
 }
